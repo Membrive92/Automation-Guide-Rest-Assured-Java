06cefae8a9b1c955094fe2892bc8d05692acb480
membri92 <membri_2@hotmail.com>
2023-08-04 23:17:20 +0200

    Setup Rest-assured and sample test

---
+++ src/test/java/com/rest/Test.java
@@ -0,0 +1,15 @@
+package com.rest;
+
+import static io.restassured.RestAssured.*;
+import static io.restassured.matcher.ResponseAwareMatcher.*;
+import static org.hamcrest.Matcher.*;
+
+public class Test {
+
+    @org.testng.annotations.Test
+    public void test(){
+        given().
+        when().
+        then();
+    }
+}
