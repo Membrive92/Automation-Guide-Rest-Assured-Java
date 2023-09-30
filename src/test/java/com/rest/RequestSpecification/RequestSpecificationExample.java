0a56d3b611d33dfe9f90376e65d41bb5fa77fd5e
membri92 <membri_2@hotmail.com>
2023-09-30 20:52:04 +0200

    Refactor to requestSpectBuilder in beforeClass method

--- src/test/java/com/rest/RequestSpecification/RequestSpecificationExample.java
+++ src/test/java/com/rest/RequestSpecification/RequestSpecificationExample.java
@@ -1,5 +1,7 @@
 package com.rest.RequestSpecification;
 
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.filter.log.LogDetail;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
 import org.testng.annotations.BeforeClass;
@@ -18,20 +20,30 @@ public class RequestSpecificationExample {
 
     @BeforeClass
     public void beforeClass() {
-        requestSpecification = with().
+     /*   requestSpecification = with().
                 baseUri("https://api.postman.com").
-                header("X-Api-Key", postmanApiKey());
+                header("X-Api-Key", postmanApiKey());*/
+
+        RequestSpecBuilder requestSpecBuilder = new RequestSpecBuilder();
+        requestSpecBuilder.setBaseUri("https://api.postman.com");
+        requestSpecBuilder.addHeader("X-Api-Key", postmanApiKey());
+        requestSpecBuilder.log(LogDetail.ALL);
+
+        requestSpecification = requestSpecBuilder.build();
+
     }
 
     @Test
     public void validate_status_code() {
-        Response response = requestSpecification.get("/workspaces").then().log().all().extract().response();
+        Response response = given(requestSpecification).
+                header("testHeader", "testValue").
+                get("/workspaces").then().log().all().extract().response();
         assertThat(response.statusCode(), is(equalTo(200)));
     }
 
     @Test
     public void validate_response_body() {
-        Response response = requestSpecification.get("/workspaces").then().log().all().extract().response();
+        Response response = given(requestSpecification).get("/workspaces").then().log().all().extract().response();
         assertThat(response.statusCode(), is(equalTo(200)));
         assertThat(response.path("workspaces[0].name"), equalTo("Team Workspace"));
     }
