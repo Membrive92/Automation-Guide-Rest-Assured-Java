20cea1ead6f46670a916973e0c3701c5ca31b218
membri92 <membri_2@hotmail.com>
2023-10-21 20:10:23 +0200

    Add serialization test using Jackson dependency - map and list to json tests

--- src/test/java/com/rest/Serialization/JacksonAPI_JSONArray.java
+++ src/test/java/com/rest/Serialization/JacksonAPI_JSONArray.java
@@ -2,7 +2,6 @@ package com.rest.Serialization;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import io.restassured.RestAssured;
 import io.restassured.builder.RequestSpecBuilder;
 import io.restassured.builder.ResponseSpecBuilder;
 import io.restassured.config.EncoderConfig;
@@ -21,46 +20,51 @@ import static io.restassured.RestAssured.*;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.matchesPattern;
 
-public class JacksonAPI_JSONObject {
+public class JacksonAPI_JSONArray {
     ResponseSpecification customResponseSpecification;
 
     @BeforeClass
     public void beforeClass() {
-        RequestSpecBuilder requestSpecBuilder = new RequestSpecBuilder();
-        requestSpecBuilder.setBaseUri("https://api.postman.com");
-        requestSpecBuilder.addHeader("X-Api-Key", postmanApiKey()).
-                setContentType(ContentType.JSON).
+        RequestSpecBuilder requestSpecBuilder = new RequestSpecBuilder().
+                setBaseUri("https://b9f7a298-1b18-4062-b3ce-6c08731ac0bd.mock.pstmn.io").
+                addHeader("x-mock-match-request-body", "true").
+                setConfig(config.encoderConfig(EncoderConfig.encoderConfig()
+                        .appendDefaultContentCharsetToContentTypeIfUndefined(false))).
+                setContentType("application/json;charset=utf-8").
                 log(LogDetail.ALL);
+
         requestSpecification = requestSpecBuilder.build();
 
         ResponseSpecBuilder responseSpecBuilder = new ResponseSpecBuilder().
                 expectStatusCode(200).
                 expectContentType(ContentType.JSON).
                 log(LogDetail.ALL);
-        RestAssured.responseSpecification = responseSpecBuilder.build();
+        customResponseSpecification = responseSpecBuilder.build();
     }
 
     @Test
-    public void validate_post_request_payload_json_array_as_map() throws JsonProcessingException {
-        HashMap<String, Object> mainObject = new HashMap<String, Object>();
+    public void validate_post_request_payload_json_as_list() throws JsonProcessingException {
+        HashMap<String, String> obj5001 = new HashMap<String, String>();
+        obj5001.put("id","5001");
+        obj5001.put("type", "None");
 
-        HashMap<String, String> nestedObject = new HashMap<String, String>();
-        nestedObject.put("name","JacksonWorkspace");
-        nestedObject.put("type", "personal");
-        nestedObject.put("description", "Rest Assured and Jackson");
+        HashMap<String, String> obj5002 = new HashMap<String, String>();
+        obj5002.put("id","5002");
+        obj5002.put("type", "Glazed");
 
-        mainObject.put("workspace", nestedObject);
+        List<HashMap<String, String>> jsonList = new ArrayList<>();
+        jsonList.add(obj5001);
+        jsonList.add(obj5002);
 
         ObjectMapper objectMapper = new ObjectMapper();
-        String mainObjectStr = objectMapper.writeValueAsString(mainObject);
+        String jsonListStr = objectMapper.writeValueAsString(jsonList);
 
         given().
-                body(mainObjectStr).
+                body(jsonListStr).
         when().
-                post("/workspaces").
-        then().spec(responseSpecification).
+                post("/post").
+        then().spec(customResponseSpecification).
                 assertThat().
-                body("workspace.name", equalTo("JacksonWorkspace"),
-                        "workspace.id", matchesPattern("^[a-z0-9-]{36}$"));
+                body("msg", equalTo("success"));
     }
 }
