9402c7838d776a1b1a8714b5dab33f8dc376fbd4
membri92 <membri_2@hotmail.com>
2023-10-28 22:01:58 +0200

    Creating Pojos from live project

---
+++ src/main/java/com/rest/pojo/ComplexLivePojo/collectionPojos/CollectionRoot.java
@@ -0,0 +1,20 @@
+package com.rest.pojo.ComplexLivePojo.collectionPojos;
+
+public class CollectionRoot {
+    Collection collection;
+
+    public Collection getCollection() {
+        return collection;
+    }
+
+    public void setCollection(Collection collection) {
+        this.collection = collection;
+    }
+
+    public CollectionRoot(Collection collection) {
+        this.collection = collection;
+    }
+
+    public CollectionRoot() {
+    }
+}
