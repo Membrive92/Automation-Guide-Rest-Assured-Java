5158be88a78eea1a78e46f300484af0860c8b6bf
membri92 <membri_2@hotmail.com>
2023-11-19 19:06:50 +0100

    Framework: Using Faker for generate random data on tests

--- src/test/java/com/rest/framework/spotify/oauth2/tests/PlaylistTests.java
+++ src/test/java/com/rest/framework/spotify/oauth2/tests/PlaylistTests.java
@@ -12,6 +12,8 @@ import org.testng.annotations.Test;
 import java.io.FileNotFoundException;
 
 import static com.rest.framework.spotify.oauth2.api.Assert.assertError;
+import static com.rest.framework.spotify.oauth2.utils.FakerUtils.generateDescription;
+import static com.rest.framework.spotify.oauth2.utils.FakerUtils.generateName;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 
@@ -27,7 +29,7 @@ public class PlaylistTests {
     @Description("Create a playlist with  New Playlist name")
     @Test(description = "should be able to create a playlist")
     public void ShouldBeAbleToCreateAPlaylist() throws FileNotFoundException {
-      Playlist requestPlaylist = new Playlist("New Playlist","New playlist description", false);
+      Playlist requestPlaylist = new Playlist(generateName(),generateDescription(), false);
       Response response = PlaylistApi.post(requestPlaylist);
       Assert.assertStatusCode(response.statusCode(), 201);
       Assert.assertPlaylistEqual(response.as(Playlist.class), requestPlaylist);
@@ -55,7 +57,7 @@ public class PlaylistTests {
     @Description("Update a playlist with Update Playlist name")
     @Test(description = "should be able to update a playlist")
     public void ShouldBeAbleToUpdateAPlaylist() throws FileNotFoundException {
-        Playlist requestPlaylist = new Playlist("Update Playlist","New Update playlist description", false);
+        Playlist requestPlaylist = new Playlist(generateName(),generateDescription(), false);;
         Response response = PlaylistApi.put(DataLoader.getInstance().getUpdatePlaylistId(), requestPlaylist);
         Assert.assertStatusCode(response.statusCode(), 200);
     }
@@ -68,7 +70,7 @@ public class PlaylistTests {
     @Description("Check that playlist should not be create with empty name")
     @Test(description = "should not be able to create a playlist")
     public void ShouldNotdBeAbleToCreateAPlaylist() throws FileNotFoundException {
-        Playlist requestPlaylist = new Playlist("","New Update playlist description", false);
+        Playlist requestPlaylist = new Playlist("",generateDescription(), false);
         Response response = PlaylistApi.post(requestPlaylist);
         Assert.assertStatusCode(response.statusCode(), 400);
         assertError(response.as(Error.class),400,"Missing required field: name");
@@ -83,7 +85,7 @@ public class PlaylistTests {
     @Test(description = "should not be  able to create a playlist with expired token")
     public void ShouldNotdBeAbleToCreateAPlaylistWithExpiredToken() throws FileNotFoundException {
         String invalidToken = "12345";
-        Playlist requestPlaylist = new Playlist("New Playlist","New playlist description", false);
+        Playlist requestPlaylist = new Playlist(generateName(),generateDescription(), false);
         Response response = PlaylistApi.post(invalidToken,requestPlaylist);
         assertThat(response.statusCode(), equalTo(401));
         assertError(response.as(Error.class),401,"Invalid access token");
