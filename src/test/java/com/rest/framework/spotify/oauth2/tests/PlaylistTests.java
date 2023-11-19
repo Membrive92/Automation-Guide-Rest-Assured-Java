a61551215d73f9385dadf9a7b1bed9cda91a0739
membri92 <membri_2@hotmail.com>
2023-11-19 16:47:22 +0100

    Framework: Add name and description in Allure report

--- src/test/java/com/rest/framework/spotify/oauth2/tests/PlaylistTests.java
+++ src/test/java/com/rest/framework/spotify/oauth2/tests/PlaylistTests.java
@@ -5,6 +5,7 @@ import com.rest.framework.spotify.oauth2.api.applicationApi.PlaylistApi;
 import com.rest.framework.spotify.oauth2.pojo.Error;
 import com.rest.framework.spotify.oauth2.pojo.Playlist;
 import com.rest.framework.spotify.oauth2.utils.DataLoader;
+import io.qameta.allure.Description;
 import io.restassured.response.Response;
 import org.testng.annotations.Test;
 
@@ -16,7 +17,8 @@ import static org.hamcrest.Matchers.equalTo;
 
 public class PlaylistTests {
 
-    @Test
+    @Description("Create a playlist with  New Playlist name")
+    @Test(description = "should be able to create a playlist")
     public void ShouldBeAbleToCreateAPlaylist() throws FileNotFoundException {
       Playlist requestPlaylist = new Playlist("New Playlist","New playlist description", false);
       Response response = PlaylistApi.post(requestPlaylist);
@@ -24,7 +26,8 @@ public class PlaylistTests {
       Assert.assertPlaylistEqual(response.as(Playlist.class), requestPlaylist);
     }
 
-    @Test
+    @Description("Get a specific playlist")
+    @Test(description = "should be able to get a playlist")
     public void ShouldBeAbleToGetAPlaylist() throws FileNotFoundException {
         Playlist requestPlaylist = new Playlist("Update Playlist","New Update playlist description", false);
         Response response = PlaylistApi.get(DataLoader.getInstance().getGetPlaylistId());
@@ -32,14 +35,16 @@ public class PlaylistTests {
         Assert.assertPlaylistEqual(response.as(Playlist.class), requestPlaylist);
     }
 
-    @Test
+    @Description("Update a playlist with Update Playlist name")
+    @Test(description = "should be able to update a playlist")
     public void ShouldBeAbleToUpdateAPlaylist() throws FileNotFoundException {
         Playlist requestPlaylist = new Playlist("Update Playlist","New Update playlist description", false);
         Response response = PlaylistApi.put(DataLoader.getInstance().getUpdatePlaylistId(), requestPlaylist);
         Assert.assertStatusCode(response.statusCode(), 200);
     }
 
-    @Test
+    @Description("Check that playlist should not be create with empty name")
+    @Test(description = "should not be able to create a playlist")
     public void ShouldNotdBeAbleToCreateAPlaylist() throws FileNotFoundException {
         Playlist requestPlaylist = new Playlist("","New Update playlist description", false);
         Response response = PlaylistApi.post(requestPlaylist);
@@ -47,7 +52,8 @@ public class PlaylistTests {
         assertError(response.as(Error.class),400,"Missing required field: name");
     }
 
-    @Test
+    @Description("Check that playlist should not be create with a expired token")
+    @Test(description = "should not be  able to create a playlist with expired token")
     public void ShouldNotdBeAbleToCreateAPlaylistWithExpiredToken() throws FileNotFoundException {
         String invalidToken = "12345";
         Playlist requestPlaylist = new Playlist("New Playlist","New playlist description", false);
